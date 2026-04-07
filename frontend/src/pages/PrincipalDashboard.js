ad5d9fc8182633c239eb811f8434c1e12b9c8ed3
Akanksh Rajana <130879151+Akanksh2144@users.noreply.github.com>
2026-04-07 18:20:37 +0530

    fix: resolve ESLint exhaustive-deps warning to pass CI build requirements

--- frontend/src/pages/PrincipalDashboard.js
+++ frontend/src/pages/PrincipalDashboard.js
@@ -86,6 +86,7 @@ const PrincipalDashboard = ({ navigate, user, onLogout }) => {
 
   useEffect(() => {
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeTab]);
 
   const fetchData = async () => {
